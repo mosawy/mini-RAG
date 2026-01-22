3302263eb80765338337657ccfffe82aec7270df
mosawy <mohamed.elsawy739@gmail.com>
2026-01-22 12:34:20 +0200

    feat: Set up FastAPI application with base router and welcome endpoint

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from routes import base
+from dotenv import load_dotenv
 
+load_dotenv(".env")
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome_message():
-    return {"message": "Welcome to the FastAPI application!"}
+
+app.include_router(base.base_router)
