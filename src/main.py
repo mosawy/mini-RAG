31b4b37878258b8605f936f029fcfda0ade89a64
mosawy <mohamed.elsawy739@gmail.com>
2026-01-22 12:53:23 +0200

    feat: Add configuration management and initial route setup for FastAPI application

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from routes import base
-from dotenv import load_dotenv
 
-load_dotenv(".env")
+
 app = FastAPI()
 
 
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI , APIRouter
-import os
-
+from helpers.config import get_settings
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"]
@@ -9,8 +8,9 @@ base_router = APIRouter(
 
 @base_router.get("/")
 async def welcome_message():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+    app_settings = get_settings()
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "app_name": app_name,
         "app_version": app_version
